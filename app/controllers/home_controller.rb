5fd864af5985b3e2b297c783451b76e112a2f2b3
David Perry <curiousepic@gmail.com>
2014-11-20 14:06:20 -0500

    Generated home controller, added set_client method

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+
+  private
+
+  def set_client
+    return unless logged_in?
+    salesforce_auth = current_user.authorizations.find_by(provider: 'salesforce')
+    return unless salesforce_auth
+    client = Restforce.new :host => 'ENV[SALESFORCE_HOST]',
+      :oauth_token => 'oauth token',
+      :refresh_token => 'refresh token',
+      :instance_url  => 'https://na17.salesforce.com',
+      :client_id     => 'ENV[SALESFORCE_KEY]',
+      :client_secret => 'ENV[SALESFORCE_SECRET]'
+    end
+end

a85da274d534cb96603f283025a3cb0440a47deb
David Perry <curiousepic@gmail.com>
2014-11-20 13:50:35 -0500

    Added sessions controller and actions, current_user methods to applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?, :current_user, :current_user=
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:current_user_id])
+  end
+
+  def current_user=(user)
+    @current_user = user
+    session[:current_user_id] = user.id
+  end
+
+  def logged_in?
+    current_user
+  end
 end

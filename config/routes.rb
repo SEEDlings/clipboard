879ab2fc75cb43ad7edf8ad6a0018d98cf2658c1
David Perry <curiousepic@gmail.com>
2014-11-20 13:42:19 -0500

    Added find or create auth hash method to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/oauth/salesforce/callback', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

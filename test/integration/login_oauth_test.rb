312ef32d8ba51d7a2b1dd52939794e1af3c6561e
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-22 16:00:42 -0500

    added salesforce host to env file set to cs21, changed instance url to env variable in home_controller

--- test/integration/login_oauth_test.rb
+++ test/integration/login_oauth_test.rb
@@ -1,5 +1,30 @@
 require 'test_helper'
 
 class LoginOauthTest < ActionDispatch::IntegrationTest
+  should "login with salesforce and return to home" do
+    background do
+      visit root_path
+      click_link 'Sign in'
 
+      OmniAuth.config.mock_auth[:facebook] = {
+          provider: 'salesforce',
+          uid: user.facebook_account.uid,
+          credentials: {
+              :oauth_token   => salesforce_auth.oauth_token,
+              :refresh_token => salesforce_auth.refresh_token,
+              :instance_url  => 'https://na17.salesforce.com',
+              :client_id     => ENV['SALESFORCE_KEY'],
+              :client_secret => ENV['SALESFORCE_SECRET']
+          }
+      }
+      end
+
+    context 'click Log in via Salesforce' do
+      click_link 'Log in via Salesforce'
+
+      assert page.has_content('Woo')
+    end
+
+  end
+  end
 end

639ecd93135451deec042d47c62a7e5685130796
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-23 13:37:48 -0500

    generated sessions controller spec to test login

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,16 +14,28 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+OmniAuth.config.test_mode = true
+omniauth_hash = { 'provider' => 'github',
+                  'uid' => '12345',
+                  'info' => {
+                      'name' => 'rebecca',
+                      'email' => 'hi@seedlings.com',
+                      'nickname' => 'RebeccaConley'
+                  },
+                  # 'extra' => {'raw_info' =>
+                  #                 { 'location' => 'San Francisco',
+                  #                   'gravatar_id' => '123456789'
+                  #                 }
+                  # }
+}
+
+OmniAuth.config.add_mock(:github, omniauth_hash)
 RSpec.configure do |config|
-  OmniAuth.config.test_mode = true
-  OmniAuth.config.mock_auth[:salesforce] = OmniAuth::AuthHash.new({
-                                                                      :provider => 'salesforce',
-                                                                      :uid => '1337',
-                                                                      :info => {
-                                                                          'name' => 'JonnieHallman',
-                                                                          'email' => 'jon@test.com'
-                                                                      }
-                                                                  })
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

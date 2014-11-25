72de3414ca7800778f01c3b3764fd0741c5f936a
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-25 09:56:54 -0500

    test action to be modified

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,7 +1,9 @@
 class VolunteersController < ApplicationController
   before_action :client
 
-  def testaction
-    puts client.search('FIND {david}')
-  end
+    def testaction
+      result = client.query("select Id from contact where LastModifiedDate < #{DateTime.now}")
+      puts result.current_page
+      binding.pry
+    end
 end

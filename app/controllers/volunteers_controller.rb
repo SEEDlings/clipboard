8fea0cbd52115b547f084ff61048f1ebfa20553f
David Perry <curiousepic@gmail.com>
2014-11-25 10:16:17 -0500

    testaction gets names in a single faster query

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -2,17 +2,14 @@ class VolunteersController < ApplicationController
   before_action :client
 
   def testaction
-    updated_ids = []
-    updated_records = []
-    updated = client.query("select Id from contact where SystemModstamp < #{DateTime.now}")
+    names = []
+
+    updated = client.query("SELECT Id, FirstName FROM Contact WHERE SystemModstamp < #{DateTime.now}")
     updated.current_page.each do |o|
-      updated_ids << o.Id
+      names << o.FirstName
     end
-    updated_ids.each do |o|
-      updated_records << client.find('Contact', "#{o}")
-    end
-    updated_records.each do |o|
-      puts o.FirstName
-    end
+
+    puts names
+
   end
 end

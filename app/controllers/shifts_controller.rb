5ec1ee45c95ba8e49437244629257ff152cb82d2
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-26 17:03:23 -0500

    confirmed button updates salesforce

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,2 +1,8 @@
 class ShiftsController < ApplicationController
+  before_action :client
+
+    def confirm
+      result = @client.update!('SEEDS_Volunteer_Shifts__c', Id: params[:shift_id], Shift_Status__c: "Confirmed")
+      redirect_to shifts_index_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'volunteers/testaction'
 
+  post 'shifts/confirm'
 
   root 'home#index'
 

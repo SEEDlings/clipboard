428120618c19876b5b6d547822afc96feae9e88a
Pete <petermason.byrne@gmail.com>
2014-11-26 14:16:36 -0500

    correctly selects and displays names for todays shifts

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,20 +1,24 @@
 class ShiftsController < ApplicationController
-
-  def shifts_day
-    @shifts = []
-    @shift = testshift
-    @shift.date = "November 26"
-    @month = Date.today.strftime("%B")
-    @day = Date.today.day
-    @today = "#{@month}" + " #{@day}"
-    if @shift.date = @today
-      render
-    else
-
-    end
-
+  def new
   end
+
+  def index
+    @shifts = Shift.all
+  end
+
+  # def shifts_day
+    # @shifts = []
+    # @shift = testshift
+    # @shift.date = "November 26"
+    # @month = Date.today.strftime("%B")
+    # @day = Date.today.day
+    # @today = "#{@month}" + " #{@day}"
+    # if @shift.date = @today
+    #   render
+    # else
+    #
+    # end
+  # end
 
     #want to display the volunteer names associated with all today's shifts
-  end
 end

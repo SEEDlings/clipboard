be0d8ae347c26703b14d0392ba74c3162c5ced2c
David Perry <curiousepic@gmail.com>
2014-11-26 15:13:40 -0500

    Moved sync method to syncer model, creates syncer on seed, fixed timestamp format

--- db/migrate/20141124201244_create_syncers.rb
+++ db/migrate/20141124201244_create_syncers.rb
@@ -1,8 +1,8 @@
 class CreateSyncers < ActiveRecord::Migration
   def change
     create_table :syncers do |t|
-      t.string :last_sync
-      t.string :last_full_sync
+      t.string :last_sync, default: "1000-01-01T00:00:00Z"
+      t.string :last_full_sync, default: "1000-01-01T00:00:00Z"
 
       t.timestamps
     end

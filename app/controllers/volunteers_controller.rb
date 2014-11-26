8ad5800951768acdccd87120cbfe4b13c2147332
David Perry <curiousepic@gmail.com>
2014-11-26 12:17:11 -0500

    sync method updating and creating volunteers and shifts correctly, added shift_name

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -22,35 +22,103 @@ class VolunteersController < ApplicationController
         WHERE SystemModstamp > #{last_sync}")
 
     updated_contacts.current_page.each do |o|
-      volunteers << {sf_id: o.Id, name_first: o.FirstName, name_last: o.LastName, email: o.Email}
+      volunteers << {sf_id: o.Id,
+                     name_first: o.FirstName,
+                     name_last: o.LastName,
+                     email: o.Email}
     end
     updated_shifts.current_page.each do |o|
       # logic to turn job type bools into a activity
-      shifts << {sf_volunteer_shift_id: o.Id, contact_name: o.Volunteer_Name__c, status: o.Shift_Status__c}
+      shifts << {sf_volunteer_shift_id: o.Id,
+                 sf_id: o.Volunteer_Name__c,
+                 status: o.Shift_Status__c}
     end
     updated_details.current_page.each do |o|
-      details << {sf_shift_detail_id: o.Id, shift: o.Shift__c, shift_name: o.Shift_Name__c, date_text: o.Date_text__c, date_calendar: o.Date_Calendar__c, hours: o.Shift_Hours__c}
+      details << {sf_shift_detail_id: o.Id,
+                  sf_volunteer_shift_id: o.Shift__c,
+                  shift_name: o.Shift_Name__c,
+                  date: "#{o.Date_text__c} or #{o.Date_Calendar__c}",
+                  hours: o.Shift_Hours__c}
     end
 
-    puts volunteers
-    # puts shifts
-    # puts details
-
     volunteers.each do |sf_v|
-      if Volunteer.any? { |ev| ev.sf_id == sf_v[:sf_id] }
-        puts "Existing record found, updating #{sf_v[:sf_id]} #{sf_v[:name_first]} #{sf_v[:name_last]}"
+      if Volunteer.any? { |e_v| e_v.sf_id == sf_v[:sf_id] }
+        puts "Existing Volunteer found, updating #{sf_v[:sf_id]} #{sf_v[:name_first]} #{sf_v[:name_last]}"
         updated_volunteer = Volunteer.find_by(sf_id: sf_v[:sf_id])
-        updated_volunteer.attribute_names.each do |a|
-          unless updated_volunteer[:a] == sf_v[:a]
-            updated_volunteer.update!(a => sf_v[:a])
-            puts "#{a} was updated."
-          end
-        end
+        updated_volunteer.update!(sf_v)
+        puts "#{updated_volunteer.name_first} was updated."
       else
-        puts "New record found, creating #{sf_v[:sf_id]} #{sf_v[:name_first]} #{sf_v[:name_last]}"
-        Volunteer.create!(sf_id: sf_v[:sf_id], name_first: sf_v[:name_first], name_last: sf_v[:name_last], email: sf_v[:email])
+        puts "No existing Volunteer found, creating Volunteer #{sf_v[:sf_id]} #{sf_v[:name_first]} #{sf_v[:name_last]}"
+        Volunteer.create!(sf_id: sf_v[:sf_id],
+                          name_first: sf_v[:name_first],
+                          name_last: sf_v[:name_last],
+                          email: sf_v[:email])
         puts "Created"
       end
     end
+
+    shifts.each do |sf_s|
+      # If there are any (COULD BE MULTIPLE) shifts with the same sf_volunteer_shift_id
+      # update the status
+      if Shift.any? { |e_s| e_s.sf_volunteer_shift_id == sf_s[:sf_volunteer_shift_id] }
+        puts "Existing Shift(s) found, updating from Volunteer Shift #{sf_s[:sf_volunteer_shift_id]}"
+        updated_shifts = Shift.where(sf_volunteer_shift_id: sf_s[:sf_volunteer_shift_id])
+        updated_shifts.each {|s| s.update!(status: sf_s[:status]) }
+        puts "status updated"
+        # !!! remove when status is moved to detail instead of vol shift
+      else
+        puts "No existing Shift found, creating Shift #{sf_s[:sf_volunteer_shift_id]}"
+        Shift.create!(sf_volunteer_shift_id: sf_s[:sf_volunteer_shift_id],
+                      sf_shift_detail_id: "pending detail",
+                      sf_id: sf_s[:sf_id],
+                      volunteer_id: Volunteer.find_by(sf_id: sf_s[:sf_id]).id,
+                      activity_id: "pending activity logic",
+                      # give activity_id when that logic is in place
+                      date: "pending detail",
+                      hours: "pending detail",
+                      shift_name: "pending detail",
+                      status: sf_s[:status] )
+        puts "Created"
+      end
+    end
+
+    details.each do |sf_d|
+      # If there is an existing shift with both the same "sf_volunteer_shift_id" AND "sf_shift_detail_id"
+      if Shift.any? { |e_s| e_s.sf_volunteer_shift_id == sf_d[:sf_volunteer_shift_id] && e_s.sf_shift_detail_id == sf_d[:sf_shift_detail_id]}
+        puts "Existing Shift found, updating from Shift Detail #{sf_d[:sf_shift_detail_id]}"
+        updated_shift = Shift.find_by(sf_shift_detail_id: sf_d[:sf_shift_detail_id])
+        updated_shift.update!(sf_d)
+        puts "#{updated_shift.sf_shift_detail_id} was updated"
+
+      # If there is an existing shift with the same "sf_volunteer_shift_id", and "sf_shift_detail_id" is "pending detail"
+      # update it with the detail info
+      elsif Shift.any? { |e_s| e_s.sf_volunteer_shift_id == sf_d[:sf_volunteer_shift_id] && e_s.sf_shift_detail_id == "pending detail"}
+        puts "Shift pending detail found, filling in pending info with Shift Detail #{sf_d[:sf_shift_detail_id]}"
+        partial_shift = Shift.find_by(sf_volunteer_shift_id: sf_d[:sf_volunteer_shift_id])
+        partial_shift.update!(sf_d)
+        puts "Pending Shift #{partial_shift.sf_shift_detail_id} was completed with detail info"
+
+      # If there is an existing shift with the same "sf_volunteer_shift_id", and "sf_shift_detail_id" is NOT "pending detail" OR nil
+      # create a new Shift with the same "sf_volunteer_shift_id" info, and the new detail info
+      elsif Shift.any? { |e_s| e_s.sf_volunteer_shift_id == sf_d[:sf_volunteer_shift_id] && e_s.sf_shift_detail_id != "pending detail" && e_s.sf_shift_detail_id != nil}
+        companion_shift = Shift.find_by(sf_volunteer_shift_id: sf_d[:sf_volunteer_shift_id])
+        Shift.create!(sf_volunteer_shift_id: companion_shift[:sf_volunteer_shift_id],
+                      sf_shift_detail_id: sf_d[:sf_shift_detail_id],
+                      sf_id: companion_shift[:sf_id],
+                      volunteer_id: companion_shift[:volunteer_id],
+                      activity_id: "pending activity logic",
+                      date: "#{sf_d[:date_text]} or #{sf_d[:date_calendar]}",
+                      hours: sf_d[:hours],
+                      shift_name: sf_d[:shift_name],
+                      status: companion_shift[:status]
+                      # will be updating status from detail here in future
+                      )
+      else
+        puts "#{sf_d[:sf_shift_detail_id]} is a Rogue Detail without associated Volunteer Shift!"
+      end
+    end
+
+
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124201244) do
+ActiveRecord::Schema.define(version: 20141126165122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20141124201244) do
     t.string   "status",                                         default: "Sign Up"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "shift_name"
   end
 
   add_index "shifts", ["activity_id"], name: "index_shifts_on_activity_id", using: :btree

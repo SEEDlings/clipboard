dfccdeb140609919c90d9047bbd9a330cea38c2b
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-26 20:00:15 -0500

    button updates status in salesforce and clipboard db

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,7 +2,10 @@ class ShiftsController < ApplicationController
   before_action :client
 
     def confirm
-      result = @client.update!('SEEDS_Volunteer_Shifts__c', Id: params[:shift_id], Shift_Status__c: "Confirmed")
-      redirect_to shifts_index_path
+      shift = params[:sf_volunteer_shift_id]
+      if @client.update!('SEEDS_Volunteer_Shifts__c', Id: "#{shift}", Shift_Status__c: "Confirmed")
+        Shift.find_by(sf_volunteer_shift_id: shift).update!(status: 'Confirmed')
+      end
+        render shifts_index_path
     end
 end

8c14d7134f5cdb0ac63b9f38bd762f3b8c174ba7
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-28 12:28:36 -0500

    check for existing record with given email works with hard coded email

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,27 +1,27 @@
-class VolunteersController < ApplicationController
+ class VolunteersController < ApplicationController
   before_action :client
 
-def testaction
-# this code checks sf to see if a contact exists then updates it
-  @lastname = 'Palin'
-  @firstname = 'Michael'
-  @email = 'michael@montypython.org'
-  @existing_records = []
-  @matching_names = []
-  @existing = @client.query("select Id from contact where Email = '@email'")
-  @existing.current_page.each do |o|
-    @existing_records << o.Id
+  def testaction
+  # this code checks sf to see if a contact exists then updates it
+      @lastname = 'Palin'
+      @firstname = 'Michael'
+      @email = 'michael@montypython.org'
+      @existing_records = []
+      @matching_names = []
+      @existing = @client.query("select Id from contact where email = '#{@email}'")
+      @existing.current_page.each do |o|
+        @existing_records << o.Id
+    end
+    @existing_records.each do |o|
+      @matching_names << client.find('Contact', "#{o}")
+    end
+      if @matching_names.empty?
+        @client.create!('Contact', FirstName: @firststname, LastName: @lastname, Email: @email )
+      else
+        puts 'We already have someone in the database with that email'
+      end
+    end
   end
-  @existing_records.each do |o|
-    @matching_names << client.find('Contact', "#{o}")
-  end
-  @matching_names.each do |o|
-    puts o.FirstName
-  end
-  @matching_names.nil?
-  @client.create!('Contact', FirstName: @firststname, LastName: @lastname, Email: @email )
-end
-end
 
 
 

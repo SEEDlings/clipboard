7ab0dd6d35ce5d03d54e5204bbac97087cfbfa09
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-28 12:55:23 -0500

    created form to add new volunteer but not mapped to sf action

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,11 +1,16 @@
  class VolunteersController < ApplicationController
   before_action :client
 
+  def new
+
+  end
+
   def testaction
   # this code checks sf to see if a contact exists then updates it
-      @lastname = 'Palin'
-      @firstname = 'Michael'
-      @email = 'michael@montypython.org'
+      @volunteer = Volunteer.new
+      @firstname = 'Terry'
+      @lastname = 'Gilliam'
+      @email = 'terry@montypython.org'
       @existing_records = []
       @matching_names = []
       @existing = @client.query("select Id from contact where email = '#{@email}'")

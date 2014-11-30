f5ef313cb18cba55ca256d67a41df5f14ee88938
David Perry <curiousepic@gmail.com>
2014-11-30 17:18:23 -0500

    Set up Foundation, added Droid webfont

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
   before_action :client
 
-def index
-  return unless logged_in?
-end
+  def index
+    return unless logged_in?
+  end
 
-def sync
-  Syncer.find_by(id: 1).syncup(@client)
-end
+  def sync
+    Syncer.find_by(id: 1).syncup(@client)
+  end
 
-private
+  private
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post 'shifts/no_show'
 
   root 'home#index'
-  get 'home/sync'
+  get 'home/sync', to: 'home#sync'
 
   get 'sessions/create'
 
